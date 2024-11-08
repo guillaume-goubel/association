42d30fd179e2cb0ae9e77c5f9a8763e0993eb003
guillaume <guillaume.goubel.pro@gmail.com>
2024-11-08 18:39:29 +0100

    event card front 4

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Event;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/blog', name: 'blog_')]
 class BlogController extends AbstractController
 {
-    #[Route('/index', name: 'index')]
-    public function index(): Response
-    {
+    #[Route('/{id}/index', name: 'index')]
+    public function index(Event $event, Request $request): Response
+    {   
+
         return $this->render('blog/post_page/index.html.twig', [
-            'is_passed' => true,
+            'is_passed' => $request->query->get('is_passed') ?? null,
+            'event' => $event
         ]);
     }
 
